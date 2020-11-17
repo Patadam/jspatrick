d637497643b4271eb06642b5e02dac1dee3603c1
Patrick <patadamrich@gmail.com>
2020-11-17 17:37:30 +1000

    Updated Sidebar
    Sidebar now includes anchor navigation.
    Animation to slide in and out now works - although is not the smoothest or best
    
    !** Note Dark theme breaks when navigating to a non-existant anchor. -- can be fixed by adding the remaining anchor sections

--- jspatrick/index.js
+++ jspatrick/index.js
@@ -9,9 +9,23 @@ function toggleTheme() {
 }
 
 document.querySelector("#container").addEventListener("click", function () {
-    document.querySelector("nav").classList.add("hidden");
+    if (!(document.querySelector("nav").classList.contains("animateHide"))) {
+        document.querySelector("nav").classList.add("animateHide");
+        document.querySelector("nav").classList.remove("animateShow");
+    };
 });
 
+for (item of document.querySelectorAll("a")) {
+    item.addEventListener("click", function () {
+        for (item of document.querySelectorAll("a")) {
+            item.setAttribute("selected", false);
+        }
+        this.setAttribute("selected", true);
+    })
+}
+
+
 function loadMenu() {
-    document.querySelector("nav").classList.remove("hidden");
+    document.querySelector("nav").classList.add("animateShow");
+    document.querySelector("nav").classList.remove("animateHide");
 }
