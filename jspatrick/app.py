6455b83ac78b5645ab5c8d0bebc990b01d556440
Patrick <patadamrich@gmail.com>
2020-12-17 13:20:40 +1000

    Added development server

--- jspatrick/app.py
+++ jspatrick/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
+# Home Page Route
 @app.route('/')
 def hello():
     return 'Hello World'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
