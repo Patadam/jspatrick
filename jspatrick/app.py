454806eba7d957d7601a488c9e2d246a4c6c7943
Patrick <patadamrich@gmail.com>
2020-12-17 13:49:25 +1000

    updated routes

--- jspatrick/app.py
+++ jspatrick/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# Dummy content
+posts = [{},{}];
 
 
 
 # Home Page Route
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', posts=posts)
 
 
 if __name__ == '__main__':
