708d7236a2b29bb1c03dbae593139243e9c3c8af
Patrick <patadamrich@gmail.com>
2020-12-20 12:07:11 +1000

    + Registration & Login Forms

--- jspatrick/app.py
+++ jspatrick/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -20,6 +20,24 @@ def about():
 def blog():
     return render_template('blog.html', style='/static/styles/blog.css', javascript='/static/javascript/blog.js')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Plase check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
